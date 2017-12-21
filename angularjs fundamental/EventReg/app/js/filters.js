70a73eb1135b00b09c84d735d47eea79c8e7002a
gagan0183 <g.deepsingh1@gmail.com>
2017-12-21 18:36:15 +0530

    create custom

--- angularjs fundamental/EventReg/app/js/filters.js
+++ angularjs fundamental/EventReg/app/js/filters.js
@@ -1,1 +1,19 @@
 'use strict';
+
+eventsApp.filter('duration', function() {
+    return function(duration) {
+        switch(duration) {
+            case 1:
+                return 'half hour';
+                break;
+            case 2:
+                return '1 hour';
+                break;
+            case 3:
+                return 'half day';
+                break;
+            case 4:
+                return 'full day';
+        }
+    }
+})
